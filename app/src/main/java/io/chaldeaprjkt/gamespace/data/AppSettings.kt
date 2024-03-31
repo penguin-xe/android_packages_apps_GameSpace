62c3c47480e1b3c98af8ddc33d20b5cfe64d3dd4
Pranav Vashi <neobuddy89@gmail.com>
2024-03-31 21:31:10 +0530

    GameSpace: Fix up cast error for list preference
    Change-Id: Icddb7d3952ef395acaf783538b1c669ea14a9316
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- app/src/main/java/io/chaldeaprjkt/gamespace/data/AppSettings.kt
+++ app/src/main/java/io/chaldeaprjkt/gamespace/data/AppSettings.kt
@@ -59,11 +59,11 @@ class AppSettings @Inject constructor(private val context: Context) {
         set(value) = db.edit().putBoolean(KEY_DANMAKU_NOTIFICATION_MODE, value).apply()
 
     var callsMode: Int
-        get() = db.getString(KEY_CALLS_MODE, "0").toInt()
+        get() = db.getString(KEY_CALLS_MODE, "0")?.toIntOrNull() ?: 0
         set(value) = db.edit().putString(KEY_CALLS_MODE, value.toString()).apply()
 
     var ringerMode: Int
-        get() = db.getString(KEY_RINGER_MODE, "3").toInt()
+        get() = db.getString(KEY_RINGER_MODE, "3")?.toIntOrNull() ?: 3
         set(value) = db.edit().putString(KEY_RINGER_MODE, value.toString()).apply()
 
     var menuOpacity: Int
